1dfe61c9f16e1fed33c47e093680306791c4f66c
Raj koli <2024.rajk@isu.ac.in>
2025-01-14 22:11:08 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,7 @@
+# Write a Python program that prompts the user for two numbers and prints their sum, difference, product, and quotient.
+num1 = float (input("Enter first number: "))
+num2 = float (input("Enter second number: "))
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num1 - num2}")
+print(f"Product: {num1 * num2}")
+print(f"Quotient: {num1 / num2:.2f}")
