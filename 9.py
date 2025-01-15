8ba956be5f70177b7f6e1262c8a062f3ea02fc66
Raj koli <2024.rajk@isu.ac.in>
2025-01-15 10:15:34 +0530

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,5 @@
+# Write a Python program that converts miles to kilometers. Take miles as user input.
+# 1 mile = 1.60 kilometers
+miles = float(input("Enter the number of miles: "))
+kilometers = miles * 1.60
+print(f"{miles} miles is equal to {kilometers} kilometers")
