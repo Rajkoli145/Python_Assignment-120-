cee200e987f55a68f79fa4c5ff6d983b40115a70
Raj koli <2024.rajk@isu.ac.in>
2025-01-15 11:19:33 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,6 @@
+# Write a program that checks if a given year is a leap year.
+year = int(input("Enter a year: "))
+if year % 4 == 0:
+            print("Leap year")
+else:
+     print("Not a leap year")
